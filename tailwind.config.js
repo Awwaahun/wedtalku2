c4c205765c3b79886f40996c62ced2960d329ef7
Awwaahun <a.cheerful97@gmail.com>
2025-11-06 15:50:43 +0700

    wedtalku2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // Font dari CSS variables
+        'heading': ['var(--font-heading)', 'serif'],
+        'body': ['var(--font-body)', 'sans-serif'],
+        'script': ['var(--font-script)', 'cursive'],
+        
+        // Alias untuk compatibility
+        'serif': ['var(--font-heading)', 'serif'],
+        'sans': ['var(--font-body)', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
